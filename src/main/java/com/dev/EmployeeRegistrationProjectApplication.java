5a956bad2ed0fc112677143668fb5916812868ef
Bachir <bafosth@yahoo.fr>
2023-09-21 15:23:29 -0400

    adding bootstrap and form content

--- src/main/java/com/dev/EmployeeRegistrationProjectApplication.java
+++ src/main/java/com/dev/EmployeeRegistrationProjectApplication.java
@@ -1,4 +1,4 @@
-package com.dev.employeeregistrationproject;
+package com.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
